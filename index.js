156fd4f3227ab32a2d18088524f07671b6683944
ardacy2112 <138930253+ardacy2112@users.noreply.github.com>
2024-01-28 13:02:26 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+require("dotenv").config();
+const JUGNU = require("./handlers/Client");
+const { TOKEN } = require("./settings/config");
+
+const client = new JUGNU();
+
+module.exports = client;
+
+client.start(TOKEN);
+
+process.on("unhandledRejection", (reason, p) => {
+  console.log(" [Error_Handling] :: Unhandled Rejection/Catch");
+  console.log(reason, p);
+});
+
+process.on("uncaughtException", (err, origin) => {
+  console.log(" [Error_Handling] :: Uncaught Exception/Catch");
+  console.log(err, origin);
+});
+
+process.on("uncaughtExceptionMonitor", (err, origin) => {
+  console.log(" [Error_Handling] :: Uncaught Exception/Catch (MONITOR)");
+  console.log(err, origin);
+});
+
+const keep_alive = require('./keep_alive.js')
+
+const express = require('express');
+const app = express();
+const port = 3100;//buraya karışmayın.
+
+app.get('/', (req, res) => res.send('we discord'));//değiştirebilirsiniz.
+
+app.listen(port, () =>
+console.log(`Bot bu adres üzerinde çalışıyor: http://localhost:${port}`)//port
+);
